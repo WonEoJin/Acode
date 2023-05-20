826e2bef4042dc1edaeabe82fbf08f90b127f015
WonEoJin <134124807+WonEoJin@users.noreply.github.com>
2023-05-20 18:27:11 +0900

    price.py

--- price.py
+++ price.py
@@ -7,15 +7,22 @@ data_path = Path() / "data"
 data_path.mkdir(parents=True, exist_ok=True)
 
 def shopping(shop_file):
-    shop_dict = {} # 생성할 사전 객체
+    shop_dict = {}  # 생성할 사전 객체
 
-    with open(data_path / shop_file, mode='r', encoding='utf-8') as f:    
-      for line in f:
-        price_of_goods = line.strip().split() 
-        if price_of_goods != []:      
-          goods, price = price_of_goods 
-        if price != shop_file[4]:
-          shop_dict[goods] = int(price.rstrip('원'))
+    # data_path 변수 정의 확인
+    if 'data_path' not in locals():
+        data_path = Path() / "data"
+
+    with open(data_path / shop_file, mode='r', encoding='utf-8') as f:
+        for line in f:
+            price_of_goods = line.strip().split()
+            if price_of_goods != []:
+                goods, price = price_of_goods
+            if price != shop_file[4]:
+                shop_dict[goods] = int(price.rstrip('원'))
+
+    return shop_dict
+
 
     return shop_dict
 
